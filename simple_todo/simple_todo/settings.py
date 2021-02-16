93201d1b9262b6a62d4a91e2d3d5d6dff6d87815
empty-works <pama.mark@gmail.com>
2021-02-16 16:51:24 +0100

    Added a simple CSS selector for the activity form.

--- simple_todo/simple_todo/settings.py
+++ simple_todo/simple_todo/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -121,5 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static/'),
+        STATIC_DIR,
         )
